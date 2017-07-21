dd619e84e10ea4fbeccffe7b51b5873540efeb81
Alex Crocker <croc122@gmail.com>
2017-07-21 18:40:22 -0400

    Removed files from gh-pages branch

--- resources/views/emails/reset.blade.php
+++
@@ -1,5 +0,0 @@
-To reset your Shulkerlist password, please click the following link:
-<br>
-<a href="{{ URL::route('reset-password', ['token' => $token]) }}">{{ URL::route('reset-password', ['token' => $token]) }}</a>
-<br>
-If you didn't request a reset, please ignore this email.
